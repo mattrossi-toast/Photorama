67337124c5fd83edfdd2f078541ce9c691dff17f
Rossi, Matthew Th <008000100@n358-c2.local>
2019-02-20 19:40:13 -0500

    Initial Commit

---
+++ Photorama/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photorama
+//
+//  Created by Rossi, Matthew Th on 2/20/19.
+//  Copyright © 2019 Rossi, Matthew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
